3c31d2b3ff2357af0856bbba512b4606680f2d77
MartinMSalas <ingmartinmsalas@gmail.com>
2024-05-25 21:01:48 -0300

    feat: adding files

---
+++ Client/src/test/java/com/mms/brewery/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms.brewery.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
